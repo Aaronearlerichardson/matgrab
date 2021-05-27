2c06b7a7d076654764859cf4ff661d99e95ed736
Aaronearlerichardson <jakdaxter31@gmail.com>
2021-05-26 20:17:36 -0400

    cleaned up2

--- matgrab.py
+++ matgrab.py
@@ -15,9 +15,6 @@ def mat2df(mat_file, var=None,filepath=None):
     if isinstance(mat_file,str):
         if os.path.isfile(mat_file):
             return mat2df(sio.loadmat(mat_file,simplify_cells=True),var,filepath=mat_file)
-    elif isinstance(mat_file, list):
-        if os.path.isfile(mat_file[0]):
-            return pd.concat([mat2df(mat,var) for mat in mat_file]).squeeze()
     elif isinstance(mat_file,dict):
         mat=mat_file
         if any("__" in i for i in list(mat)) or any("readme" in i for i in list(mat)):
@@ -45,5 +42,8 @@ def mat2df(mat_file, var=None,filepath=None):
         else:
             raise ValueError("None of the vars {vars} were found in {file}".format(vars=var,file=filepath))
     elif isinstance(mat_file,list):
-        mat = pd.DataFrame(mat_file)
-        return mat.filter(list(set(var).intersection(mat.columns.tolist()))).reset_index(drop=True).squeeze()
+        if os.path.isfile(mat_file[0]):
+            return pd.concat([mat2df(mat,var) for mat in mat_file]).squeeze()
+        else:
+            mat = pd.DataFrame(mat_file)
+            return mat.filter(list(set(var).intersection(mat.columns.tolist()))).reset_index(drop=True).squeeze()

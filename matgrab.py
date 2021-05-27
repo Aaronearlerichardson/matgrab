5330adffc53bc2901b85066220a4780b490da560
Aaronearlerichardson <jakdaxter31@gmail.com>
2021-05-26 20:18:21 -0400

    cleaned up3

--- matgrab.py
+++ matgrab.py
@@ -43,7 +43,7 @@ def mat2df(mat_file, var=None,filepath=None):
             raise ValueError("None of the vars {vars} were found in {file}".format(vars=var,file=filepath))
     elif isinstance(mat_file,list):
         if os.path.isfile(mat_file[0]):
-            return pd.concat([mat2df(mat,var) for mat in mat_file]).squeeze()
+            return pd.concat([mat2df(mat,var) for mat in mat_file],axis=1).squeeze()
         else:
             mat = pd.DataFrame(mat_file)
             return mat.filter(list(set(var).intersection(mat.columns.tolist()))).reset_index(drop=True).squeeze()

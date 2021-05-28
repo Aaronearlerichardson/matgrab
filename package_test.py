bf8d0ed29aac2fecd1385964773e5735601a69eb
Aaronearlerichardson <jakdaxter31@gmail.com>
2021-05-28 13:02:42 -0400

    pypi test build

---
+++ package_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from src.matgrab import mat2df
+import os
+
+def test1():
+    data_dir = os.listdir("tests/data")
+    vars = ["Subject", "Trial", "Rec", "Day", "FilenamePrefix", "Start",
+    "Auditory", "Go", "StartCode", "AuditoryCode", "GoCode", "Noisy", "NoResponse",
+    "ResponseStart","ResponseEnd","cue", "sound", "go", "cueTime", "block", "cueStart",
+     "Trigger", "cueEnd", "audioStart", "audioAlignedTrigger", "delStart", "delEnd",
+      "goStart", "goEnd", "respStart", "respEnd", "isiStart", "isiEnd", "tCaptureStart"]
+    return mat2df(data_dir,vars)
+
+def test2():
+    data_dir = os.listdir("tests/data")
+    vars = ["channels.name","recording.sample_rate"]
+    return mat2df(data_dir,vars)
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MatGrab-Aaronearlerichardson",
+    version="0.0.4",
+    author="Aaronearlerichardson",
+    author_email="jakdaxter31@gmail.com",
+    description="search and extract field names or variables from MATLAB .mat files and return a pandas Dataframe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aaronearlerichardson/matgrab",
+    project_urls={
+        "Bug Tracker": "https://github.com/Aaronearlerichardson/matgrab/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
